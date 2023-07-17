412d69aad48bfe4e278b811f8fc14ea66d6aa359
brysonm96 <brysonmouser@gmail.com>
2023-07-17 13:51:53 -0600

    add img

--- menu.js
+++ menu.js
@@ -5,8 +5,15 @@ var itemInfo = document.getElementById('item-info');
 var itemData = {
   name: 'Example Item',
   price: 9.99,
-  description: 'This is an example item description.'
+  description: 'This is an example item description.',
+  image: 'https://example.com/image.jpg'
 };
+
+// Populate the image section
+var image = document.createElement('img');
+image.src = itemData.image;
+image.alt = itemData.name;
+imageSection.appendChild(image);
 
 // Populate the div with item information
 itemInfo.innerHTML = `
